54fb7373698132494b4d3b848fbca37779d78520
salee2 <hleesa>
2023-02-11 14:52:30 +0900

    [fix] c file name edit

--- philo/philo.c
+++ philo/philo.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_thread.c                                     :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salee2 <salee2@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 17:39:56 by salee2            #+#    #+#             */
-/*   Updated: 2022/12/07 17:39:59 by salee2           ###   ########.fr       */
+/*   Created: 2023/02/11 14:51:16 by salee2            #+#    #+#             */
+/*   Updated: 2023/02/11 14:51:17 by salee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	init_philo_thread(t_philo *philo, t_common_philo *common)
 		philo[i].nth_philo = i + 1;
 		philo[i].left_fork = i;
 		philo[i].right_fork = (i + 1) % i_end;
-		philo[i].num_of_eat = 0LL;
+		philo[i].num_of_eat = 0;
 		philo[i].last_eat_usec = get_usec();
 		philo[i].error_usec = 0LL;
 		if (pthread_mutex_init(&philo[i].last_eat_mtx, NULL))
